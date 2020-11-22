ead74dcdff87db9ef376644ef22fa23c758e463f
Rafael Gomes dos Santos <74833822+rafaelgomesdsantos@users.noreply.github.com>
2020-11-22 03:50:16 -0300

    Update Jogo do Número Secreto

--- jogonumerosecreto.py
+++ jogonumerosecreto.py
@@ -5,10 +5,10 @@ from random import *
 rdn = randint(0,20)
 tent = 5
 print("Olá jogador, duvido você acertar o número que estou pensando\n")
-print("Você tem apenas 5 vidas, Boa Sorte! >:D\n")
-print("Para começar gostaria de saber o seu nome\n")
+print("Você tem apenas 5 vidas, Boa Sorte! >:D \n")
+print("Para começar gostaria de saber o seu nome \n")
 nome = input ("Digite o seu nome: ")
-print("\nOlá", nome,", você pode me chamar de Botlomeu, vamos começar?")
+print("\n Olá", nome,", você pode me chamar de Botlomeu, vamos começar?")
 while tent > 0:
     tent = tent - 1
     ch = int(input("Tente a sorte, digite um número: "))
@@ -22,12 +22,12 @@ while tent > 0:
         print("Você tem %d vidas" % tent, "," ,nome)
         print()
     else:
-        print("Você ganhou! O número que eu estava pensando era: %d !" % rdn, " Parabéns", nome, ":D")
+        print("Você ganhou! O número que eu estava pensando era %d !" % rdn, " Parabéns", nome, ":D")
         tent = -1
 if tent == 0:
     print()
     print("Você perdeu! Mais sorte da próxima vez", nome, ":(")
-    print("o número que eu estava pensando era %d " % rdn)
+    print("o número que eu estava pensando era %d !" % rdn)
     input()
 elif tent == -1:
     input()
